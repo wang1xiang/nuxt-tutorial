cbded49183909853914350597e9aff8a7aadc26d
wangxiang <wangxiang@szkingdom.com>
2021-03-16 09:01:03 +0800

    嵌套路由

---
+++ pages/users.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>User父路由</h1>
+    <!-- 子路由出口 -->
+    <nuxt-child />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Users'
+}
+
+</script>
+<style>
+</style>
---
+++ pages/users/foo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>user - foo</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Foo'
+}
+
+</script>
+<style>
+</style>
