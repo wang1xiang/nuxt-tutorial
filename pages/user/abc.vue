a5146583861dbcede56cbb1bfb1a375c6789c110
wangxiang <wangxiang@szkingdom.com>
2021-03-16 08:45:30 +0800

    基本路由

---
+++ pages/user/abc.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>user-abc</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserAbc'
+}
+
+</script>
+<style>
+</style>
---
+++ pages/user/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>User</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserPage'
+}
+
+</script>
+<style>
+</style>
