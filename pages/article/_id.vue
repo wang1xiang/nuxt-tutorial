d5c0c4e6751a31dbe64e8d0f05fae741a3ff0827
wangxiang <wangxiang@szkingdom.com>
2021-03-17 08:58:10 +0800

    上下文对象

---
+++ pages/article/_id.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h1>{{ article.title }}</h1>
+    <div>{{ article.body }}</div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'articlePage',
+  // context上下文对象
+  // $config: {app: {…}}
+  // app: {head: {…}, router: VueRouter, nuxt: {…}, render: ƒ, data: ƒ, …}
+  // base: "/"
+  // env: {}
+  // error: ƒ ()
+  // from: {name: "index", meta: Array(1), path: "/", hash: "", query: {…}, …}
+  // isDev: true
+  // isHMR: false
+  // isStatic: false
+  // next: ƒ ()
+  // nuxtState: {layout: "default", data: Array(1), fetch: {…}, error: null, serverRendered: true, …}
+  // params: {id: "1"}
+  // payload: undefined
+  // query: {}
+  // redirect: ƒ (status, path, query)
+  // route: {name: "article-id", meta: Array(1), path: "/article/1", hash: "", query: {…}, …}
+  // _errored: false
+  // _redirected: false
+  async asyncData (context) {
+    console.log(context)
+    const { data } = await axios({
+      method: 'GET',
+      url: 'http://localhost:3000/data.json'
+    })
+    const id = Number.parseInt(context.params.id)
+    return {
+      article: data.posts.find(item => item.id === id)
+    }
+  }
+}
+
+</script>
+<style>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,28 @@
 <template>
   <div>
     <h1>hello nuxt.js</h1>
+    <br>
+    <foo :posts="posts" />
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import Foo from '@/components/Foo'
+
 // 路由规则 自动扫描pages目录，将里面的组件自动提取为路由配置
 export default {
-  name: 'HomePage'
+  name: 'HomePage',
+  components: {
+    Foo
+  },
+  async asyncData () {
+    const res = await axios({
+      method: 'GET',
+      url: 'http://localhost:3000/data.json'
+    })
+    return res.data
+  }
 }
 
 </script>
