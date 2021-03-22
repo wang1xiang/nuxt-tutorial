122ea1d36df543c91c5d7cf8f6345295d7600c9e
wangxiang <wangxiang@szkingdom.com>
2021-03-22 19:56:13 +0800

    添加个人设置页面

--- api/user.js
+++ api/user.js
@@ -18,3 +18,20 @@ export const register = data => {
     data
   })
 }
+//获取当前登陆用户
+export const getCurrentUser = () => {
+  return request({
+    method: 'GET',
+    url: '/api/user',
+  })
+}
+
+
+// 更新用户信息
+export const updateUser = data => {
+  return request({
+    method: 'PUT',
+    url: '/api/user',
+    data,
+  })
+}
